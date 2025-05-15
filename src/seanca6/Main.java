0f5afce5d685429afa3a4f58a1a7c3ea42356e4e
bjon-2 <gjermeni4bjon@gmail.com>
2025-05-15 18:44:42 +0200

    shtova nje prezantim

--- src/seanca6/Main.java
+++ src/seanca6/Main.java
@@ -3,6 +3,7 @@ package seanca6;
 public class Main {
     public static void main(String[] args){
         System.out.println("Hello from Geri");
+        System.out.println("Hello nga Bjoni");
 
     }
 }
